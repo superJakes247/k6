b8aa8cf3c89f5311a668fe3e3b7ba41ea2487066
Justin Major <justin.major@allangray.co.za>
2023-07-11 12:40:54 +0200

    fix: add mongo db performance tests and manual run job2

---
+++ __load_performance__/perf-test-doc-store-go.js
@@ -0,0 +1,67 @@
+/* eslint-disable import/extensions */
+/* eslint-disable import/no-unresolved */
+import http from 'k6/http';
+import { SharedArray } from 'k6/data';
+import { check, group, sleep } from 'k6';
+import { textSummary } from 'https://jslib.k6.io/k6-summary/0.0.1/index.js';
+import { randomIntBetween } from 'https://jslib.k6.io/k6-utils/1.2.0/index.js';
+
+export const options = {
+  vus: 100, // number of virtual users
+  iterations: 1000, // run each endpoint 1000 times
+};
+
+// export const options = {
+//   discardResponseBodies: true,
+//   scenarios: {
+//     documentStoreRampUp: {
+//       executor: 'ramping-vus',
+//       startVUs: 0,
+//       stages: [
+//         { duration: '20s', target: 40 },
+//         { duration: '20s', target: 100 },
+//       ],
+//       gracefulRampDown: '3s',
+//     },
+//   },
+// };
+
+const dataFile = new SharedArray('filesInMongoOnly', function () {
+  return JSON.parse(open('./files-in-mongo-only.json'));
+});
+
+export default function test() {
+  
+  group('api-document-store-go | get document file stored in Mongo', () => {
+
+    var varDoc = randomIntBetween(1, 2000);
+
+    const url = 'https://api.uat.gray.net/document-store/documents/document/' + dataFile[varDoc].documentid;
+
+    const response = http.get(url);
+    sleep(Math.random() * 2);
+    check(response, {
+      'is status 200': (r) => r.status === 200,
+    });
+  });
+
+  group('api-document-store-go | get document metadata', () => {
+
+    var varMetadata = randomIntBetween(1, 2000);
+
+    const url = 'https://api.uat.gray.net/document-store/documents/document/' + dataFile[varMetadata].documentid + '/metadata';
+
+    const response = http.get(url);
+    sleep(Math.random() * 2);
+    check(response, {
+      'is status 200': (r) => r.status === 200,
+    });
+  });
+
+}
+
+export function handleSummary(data) {
+  return {
+    stdout: textSummary(data, { indent: ' ', enableColors: true }), // Show the text summary to stdout...
+  };
+}
