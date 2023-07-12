d69d9bf656ce1cff8155002d51cdc4f8057f3b77
Justin Major <justin.major@allangray.co.za>
2023-07-12 17:21:06 +0200

    fix: created individual tests per test case

---
+++ __load_performance__/perf-test-docstore-file-download-aws.js
@@ -0,0 +1,49 @@
+/* eslint-disable import/extensions */
+/* eslint-disable import/no-unresolved */
+import http from 'k6/http';
+import { SharedArray } from 'k6/data';
+import { check, group, sleep } from 'k6';
+import { textSummary } from 'https://jslib.k6.io/k6-summary/0.0.1/index.js';
+import { randomIntBetween } from 'https://jslib.k6.io/k6-utils/1.2.0/index.js';
+
+export const options = {
+  discardResponseBodies: true,
+  scenarios: {
+    documentStoreRampUp: {
+      executor: 'ramping-vus',
+      startVUs: 10,
+      stages: [
+        { duration: '1m', target: 40 },
+        { duration: '1m', target: 100 },
+      ],
+      gracefulRampDown: '3s',
+    },
+  },
+};
+
+const dataFile = new SharedArray('filesInAWSOnly', function () {
+  return JSON.parse(open('./files-in-aws-only.json'));
+});
+
+export default function test() {
+  
+  group('api-document-store-go | get document file stored in AWS S3', () => {
+
+    var varDoc = randomIntBetween(1, 2000);
+
+    const url = 'https://api.uat.gray.net/document-store/documents/document/' + dataFile[varDoc].documentid;
+
+    const response = http.get(url);
+    sleep(Math.random() * 2);
+    check(response, {
+      'is status 200': (r) => r.status === 200,
+    });
+  });
+
+}
+
+export function handleSummary(data) {
+  return {
+    stdout: textSummary(data, { indent: ' ', enableColors: true }), // Show the text summary to stdout...
+  };
+}
--- __load_performance__/perf-test-docstore-query-metadata.js
+++ __load_performance__/perf-test-docstore-query-metadata.js
@@ -7,43 +7,25 @@ import { textSummary } from 'https://jslib.k6.io/k6-summary/0.0.1/index.js';
 import { randomIntBetween } from 'https://jslib.k6.io/k6-utils/1.2.0/index.js';
 
 export const options = {
-  vus: 100, // number of virtual users
-  iterations: 1000, // run each endpoint 1000 times
+  discardResponseBodies: true,
+  scenarios: {
+    documentStoreRampUp: {
+      executor: 'ramping-vus',
+      startVUs: 10,
+      stages: [
+        { duration: '1m', target: 40 },
+        { duration: '1m', target: 100 },
+      ],
+      gracefulRampDown: '3s',
+    },
+  },
 };
 
-// export const options = {
-//   discardResponseBodies: true,
-//   scenarios: {
-//     documentStoreRampUp: {
-//       executor: 'ramping-vus',
-//       startVUs: 0,
-//       stages: [
-//         { duration: '20s', target: 40 },
-//         { duration: '20s', target: 100 },
-//       ],
-//       gracefulRampDown: '3s',
-//     },
-//   },
-// };
-
-const dataFile = new SharedArray('filesInMongoOnly', function () {
+const dataFile = new SharedArray('filesInMongoMetadata', function () {
   return JSON.parse(open('./files-in-mongo-only.json'));
 });
 
 export default function test() {
-  
-  group('api-document-store-go | get document file stored in Mongo', () => {
-
-    var varDoc = randomIntBetween(1, 2000);
-
-    const url = 'https://api.uat.gray.net/document-store/documents/document/' + dataFile[varDoc].documentid;
-
-    const response = http.get(url);
-    sleep(Math.random() * 2);
-    check(response, {
-      'is status 200': (r) => r.status === 200,
-    });
-  });
 
   group('api-document-store-go | get document metadata', () => {
 
