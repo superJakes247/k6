1358c0aa255fa6182a08a2f12b09c573a61b5e7d
Shakes Ramphal <shekhar.ramphal@allangray.co.za>
2023-07-27 11:37:53 +0200

    fix: add in uat benchmarking scripts

--- README.md
+++ README.md
@@ -6,11 +6,20 @@ repo to try out K6 testing
 ## Run tests
 
 ```shell
-docker-compose run performance run /scripts/perf-test.js 
+docker-compose run performance k6 run /scripts/perf-test.js 
+```
+
+## Reporting
+
+95 percentile of all successfull results
+```
+docker-compose run testtools node results.js 
+docker-compose run performance node ./results.js  
 ```
 
 ## Run with CSV results
 
 ```shell
- docker-compose run performance run  --out csv=/results/test_results.csv /scripts/perf-test.js 
+ docker-compose run performance k6 run  --out csv=/results/test_results.csv /scripts/perf-test.js 
+ docker-compose run performance k6 run  --out csv=/results/test_results-slow-endpoints-uat-onprem.csv --tag run=uat-onprem /scripts/perf-test-slow-endpoints.js 
 ```
--- create.js
+++ create.js
@@ -1,24 +1,27 @@
 /* eslint-disable import/no-extraneous-dependencies */
 const stringComparison = require('string-comparison');
 const fs = require('fs-extra');
+const qs = require('qs');
 
 // use by cosine
 const cos = stringComparison.cosine;
 
 /* eslint-disable no-underscore-dangle */
-const json = require('./kibana-logs.json');
+const json = require('./kibana-logs-deps.json');
 
 const { hits } = json.hits;
 
-const threads = hits.map((h) => `${h._source.kubernetes.name} | ${h._source?.thread_name?.split(' - ')[1].replace('GET ', '')}`);
+const threads = hits.map((h) => ({ api: h._source.kubernetes.name, url: h._source.path || '', qs: h._source?.query_string || '' }));
 
-const unique = [...new Set(threads)].filter((f) => f !== undefined).sort();
+// const threads = hits.map((h) => `${h._source.kubernetes.name} | ${h._source?.thread_name?.split(' - ')[1].replace('GET ', '')}`);
+
+const unique = [...new Set(threads.map((t) => JSON.stringify(t)))].sort();
 
 const checkMatch = unique.map((u, i) => {
 //   console.log(u, ((i + 1) < unique.length) ? unique[i + 1] : 'x');
 
   //   console.log(cos.similarity(u, ((i + 1) < unique.length) ? unique[i + 1] : 'x'));
-  if (cos.similarity(u, ((i + 1) < unique.length) ? unique[i + 1] : 'x') < 0.75) {
+  if (cos.similarity(u, ((i + 1) < unique.length) ? unique[i + 1] : 'x') < 0.85) {
     return u;
   }
 
@@ -26,13 +29,19 @@ const checkMatch = unique.map((u, i) => {
 });
 
 const cleanList = checkMatch.filter(Boolean).filter((f) => !f.includes('undefined'));
-console.log(JSON.stringify(cleanList, null, 2));
+// console.log(JSON.stringify(cleanList, null, 2));
 
-const writeData = cleanList.map((url) => {
-  const data = `group('${url.slice(0, 150)}', () => {
-        const url = 'https://api.uat.gray.net${url.split('|')[1].replace(' ', '')}';
-    
-        const response = http.get(url);
+const writeData = cleanList.map((thre) => {
+  const threa = JSON.parse(thre);
+  const data = `group('${threa?.url.slice(0, 150)}', () => {
+        const url = new URL('${threa.url}');
+
+        ${Object.entries(qs.parse(threa.qs)).map((t) => {
+    const [k, v] = t;
+    return `url.searchParams.append('${k}', '${v}');`;
+  }).join('\n')}
+
+        const response = http.get(url.toString());
         sleep(Math.random() * 5);
         check(response, {
           'is status 200': (r) => r.status === 200,
@@ -40,5 +49,7 @@ const writeData = cleanList.map((url) => {
       }); \n`;
   return data;
 });
+
+console.log(writeData.length);
 
 fs.outputFileSync('./generated-tests.js', writeData.join('\n'));
