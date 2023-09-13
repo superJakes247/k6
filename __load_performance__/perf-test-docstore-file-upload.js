864df4ef22295d94b2df0620f1c1435c6336a8bf
Justin Major <justin.major@allangray.co.za>
2023-09-13 16:41:35 +0200

    feature: added post load test

--- __load_performance__/perf-test-docstore-file-upload.js
+++ __load_performance__/perf-test-docstore-file-upload.js
@@ -10,14 +10,15 @@ import { FormData } from 'https://jslib.k6.io/formdata/0.0.2/index.js';
 // const dataFile = new SharedArray('filesInMongoOnly', function () {
 //   return JSON.parse(open('./files-in-siebel-only.json'));
 // });
+const binFile = open('./PDFs/Ad hoc Savings Gray Issue Feb 2007.pdf', 'b');
 
 export const options = {
   scenarios: {
     linearTest: {
       executor: 'shared-iterations',
-      vus: 10,
-      iterations: dataFile.length,
-      maxDuration: "6m",
+      vus: 1,
+      iterations: 1,
+      maxDuration: '6m',
     },
   },
 };
@@ -26,31 +27,28 @@ export default function test() {
   
   group('api-document-store-go | get document file stored in Siebel', () => {
 
-    var myHeaders = new Headers();
-    myHeaders.append("Content-Type", "multipart/form-data");
-    myHeaders.append("Accept", "application/json");
-
     var form = new FormData();
-    form.append("entityType", "organisationId");
-    form.append("entityId", "justinUnitTest" + [scenario.iterationInTest]);
-    form.append("file", fileInput.files[0], "/media/sf_VMShare/PDFs/Ad hoc Savings Gray Issue Feb 2007.pdf");
-    form.append("mimeContentType", "application/octet-stream");
-    form.append("category", "TestFile");
-    form.append("subCategory", "TestFileSubType");
-    form.append("externalId", "");
-    form.append("sourceSystem", "tool-api-performance-test");
-    form.append("expiryDate", "");
-    form.append("isSiebel", "false");
-    form.append("isConfidential", "false");
-    form.append("isStaffConfidential", "false");
-    form.append("status", "ACTIVE");
-    form.append("lastUpdatedBy", "tool-api-performance-test");
+    form.append('entityType', 'organisationId');
+    form.append('entityId', 'justinUnitTest' + [scenario.iterationInTest]);
+    form.append('file', http.file(binFile, 'Ad hoc Savings Gray Issue Feb 2007.pdf'));
+    form.append('mimeContentType', 'application/octet-stream');
+    form.append('category', 'TestFile');
+    form.append('subCategory', 'TestFileSubType');
+    form.append('externalId', '');
+    form.append('sourceSystem', 'tool-api-performance-test');
+    form.append('expiryDate', '');
+    form.append('isSiebel', 'false');
+    form.append('isConfidential', 'false');
+    form.append('isStaffConfidential', 'false');
+    form.append('status', 'ACTIVE');
+    form.append('lastUpdatedBy', 'tool-api-performance-test');
 
-    const response = http.post("https://api.uat.gray.net/document-store//documents/document", 
-      FormData.body(),
-      {headers: { 'Content-Type': 'multipart/form-data; boundary=' + FormData.boundary }
+
+    const response = http.post('https://api.uat.gray.net/document-store//documents/document', form.body(), {
+      headers: { 'Content-Type': 'multipart/form-data; boundary=' + form.boundary },
     });
     sleep(Math.random() * 2);
+    console.log(response.body);
     check(response, {
       'is status 200': (r) => r.status === 200,
     });
