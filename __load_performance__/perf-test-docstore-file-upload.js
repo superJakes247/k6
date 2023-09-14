b7bc9514f16a06c27dbbc4862098cba5566472e9
Justin Major <justin.major@allangray.co.za>
2023-09-14 08:48:44 +0200

    feature: load tests for PDFs

--- __load_performance__/perf-test-docstore-file-upload.js
+++ __load_performance__/perf-test-docstore-file-upload.js
@@ -5,24 +5,27 @@ import { SharedArray } from 'k6/data';
 import { check, group, sleep } from 'k6';
 import { textSummary } from 'https://jslib.k6.io/k6-summary/0.0.1/index.js';
 import { scenario } from 'k6/execution';
+import encoding from 'k6/encoding';
 
 const Mainlist = JSON.parse(open('./files-pdf.json'));
+
 const dataFile = new SharedArray('PDFFiles', function () {
   const list = JSON.parse(open('./files-pdf.json'));
   const requests = [];
   list.forEach(filePath => {
-    requests.push(open(`./PDFs/${filePath.fileLocation}`, 'b'))
+    const binFile = open(`./PDFs/${filePath.fileLocation}`, 'b');
+    const encFile = encoding.b64encode(binFile, 'url');
+    requests.push(encFile)
   });
   return requests;
 });
-//const fileName = open('./PDFs/Ad hoc Savings Gray Issue Feb 2007.pdf', 'b');
 
 export const options = {
   scenarios: {
     linearTest: {
       executor: 'shared-iterations',
-      vus: 1,
-      iterations: 3,
+      vus: 10,
+      iterations: Mainlist.length,
       maxDuration: '6m',
     },
   },
@@ -39,14 +42,14 @@ export default function test() {
       subCategory: 'Test Sub Cat',
       entityType: 'serviceRequestNumber',
       entityId: 'LoadDocumentPOST',
-      externalId: 'Test',
+      externalId: 'Test' + [scenario.iterationInTest],
       expiryDate: '2033-11-30T18:46:19.123',
       isConfidential: 'false',
       isStaffConfidential: 'false',
-      sourceSystem: 'regression-suite',
+      sourceSystem: 'tool-api-performance-tes',
       lastUpdatedBy: 'JUSTINMAJ',
       status: 'ACTIVE',
-      file: http.file(dataFile[scenario.iterationInTest], Mainlist[scenario.iterationInTest].fileLocation, 'application/pdf'),
+      file: http.file(encoding.b64decode(dataFile[scenario.iterationInTest],'url'), Mainlist[scenario.iterationInTest].fileLocation, 'application/pdf'),
     };
 
     const headers = {
@@ -57,7 +60,7 @@ export default function test() {
     const response = http.post('https://api.uat.gray.net/document-store/documents/document', data, headers);
 
     sleep(2);
-    console.log(response.body);
+    //console.log(response.body);
     check(response, {
       'is status 200': (r) => r.status === 200,
     });
